3478429e0d80917b0c998ea9fba081661c489d06
BigBrainAFK <tony.langhammer@yahoo.de>
2021-05-26 21:14:46 +0200

    Fetch process name

--- src/win_native.c
+++ src/win_native.c
@@ -8,8 +8,8 @@ void initialize_event_hook()
     event_hook = SetWinEventHook(
         EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND,  	// Event Range to handle
         NULL,
-        event_handler,										// callback function
-        0, 0,              									// process and thread IDs of interest (0 = all)
+        event_handler,                                      // callback function
+        0, 0,                                               // process and thread IDs of interest (0 = all)
         0 	                                                // flags
     );
 }
@@ -23,14 +23,26 @@ void CALLBACK event_handler(HWINEVENTHOOK hook, DWORD event, HWND hwnd, LONG idO
                                 DWORD dwEventThread, DWORD dwmsEventTime)
 {
     LPSTR title = malloc(256);
-    GetWindowTextA(hwnd, title, 256);
+    LPSTR proc_path = malloc(2048);
+    GetWindowText(hwnd, title, 256);
 
     DWORD tid, pid;
     tid = GetWindowThreadProcessId(hwnd, &pid);
 
+    HANDLE hProc = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ , FALSE, pid);    
+    GetModuleFileNameEx(hProc, NULL, proc_path, 2048);
+
+    LPSTR proc_title = last_occurence((char*)proc_path, '\\')+1;
+
+    CloseHandle(hProc);
+
 #ifdef _DEBUG
-    printf("%s, %d, %d\n", title, tid, pid);
+    printf("%s, %d, %s, %d\n", title, tid, proc_title, pid);
 #endif
+
+    free(title);
+    free(proc_path);
+    free(&pid);
 }
 
 void start_listening()
@@ -43,4 +55,17 @@ void start_listening()
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+}
+
+char* last_occurence(char* str, char chr)
+{
+    int i, index;
+    for(i = 0; i <= strlen(str); i++)
+    {
+        if(str[i] == chr)
+        {
+            index = i;
+        }
+    }
+    return &str[index];
 }
--- src/win_native.h
+++ src/win_native.h
@@ -1,8 +1,9 @@
 #include "main.h"
-#include <stdbool.h>
 #include <windows.h>
 #include <winuser.h>
+#include <psapi.h>
 
 void start_listening();
 void CALLBACK event_handler(HWINEVENTHOOK hook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, 
                                 DWORD dwEventThread, DWORD dwmsEventTime);
+char* last_occurence(char* str, char chr);
