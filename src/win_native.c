794108ba08a568a2eefe6baaaf06e8778bd16752
BigBrainAFK <tony.langhammer@yahoo.de>
2021-05-28 22:38:35 +0200

    Fix early return call preventing free()

--- src/win_native.c
+++ src/win_native.c
@@ -50,12 +50,9 @@ void CALLBACK event_handler(HWINEVENTHOOK hook, DWORD event, HWND hwnd, LONG idO
         int match_found = 0;
 
         match_found = update_profile(title);
-        if (match_found == 1)
-            return;
-        
-        match_found = update_profile(proc_title);
-        if (match_found == 1)
-            return;
+
+        if (match_found == 0)
+            match_found = update_profile(proc_title);
     }
 
     free(title);
